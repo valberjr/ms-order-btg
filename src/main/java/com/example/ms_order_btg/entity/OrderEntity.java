d805876d6db58b43e1181a220ce8afcc6b90b57b
valberjr <valber@duck.com>
2024-07-12 16:45:18 -0300

    add mapping between Order and OrderItem entities

---
+++ src/main/java/com/example/ms_order_btg/entity/OrderEntity.java
@@ -0,0 +1,57 @@
+package com.example.ms_order_btg.entity;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Document("orders")
+public class OrderEntity {
+
+    @MongoId
+    private Long orderId;
+
+    @Indexed(name = "customer_id_index")
+    private Long customerId;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal total;
+
+    private List<OrderItem> items;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/com/example/ms_order_btg/entity/OrderItem.java
@@ -0,0 +1,40 @@
+package com.example.ms_order_btg.entity;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private String product;
+
+    private Integer quantity;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
