332e0530b409a8a47591dfd9976140c77eb23356
valberjr <valber@duck.com>
2024-07-12 15:57:48 -0300

    first commit

---
+++ src/main/java/com/example/ms_order_btg/MsOrderBtgApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_order_btg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderBtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrderBtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ms_order_btg/MsOrderBtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_order_btg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderBtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
